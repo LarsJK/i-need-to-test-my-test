d81c04715ff0d50c96b99b93df5b3686e48dd508
Lars-Jørgen Kristiansen <larsjk.84@gmail.com>
2014-09-08 13:18:52 +0200

    updated to latest ember-cli 0.0.43

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,8 +1,6 @@
-/* global require */
-
-var Application = require('route-test-problem/app')['default'];
-var Router = require('route-test-problem/router')['default'];
 import Ember from 'ember';
+import Application from 'route-test-problem/app';
+import Router from 'route-test-problem/router';
 
 export default function startApp(attrs) {
   var App;
@@ -10,7 +8,7 @@ export default function startApp(attrs) {
   var attributes = Ember.merge({
     // useful Test defaults
     rootElement: '#ember-testing',
-    LOG_ACTIVE_GENERATION:false,
+    LOG_ACTIVE_GENERATION: false,
     LOG_VIEW_LOOKUPS: false
   }, attrs); // but you can override;
 
@@ -18,7 +16,7 @@ export default function startApp(attrs) {
     location: 'none'
   });
 
-  Ember.run(function(){
+  Ember.run(function() {
     App = Application.create(attributes);
     App.setupForTesting();
     App.injectTestHelpers();
